9cc3751cf9b91de567bd7c8aebe03cef755ff5d2
Mooenz <Mooenz@users.noreply.github.com>
2021-10-16 00:49:29 +0000

    deploy: ce7ec1b0f9643be8cc9b0defd1d8168d0b19b489

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,40 +1,39 @@
-const CONTENEDOR__VIDEO = document.querySelector('.main-container__video');
-const VIDEO = document.querySelector('.video');
+const CONTENEDOR__VIDEO = document.querySelector(".main-container__video");
+const VIDEO = document.querySelector(".video");
 
 function showControls() {
-  CONTENEDOR__VIDEO.classList.add('container-video-show');
+  CONTENEDOR__VIDEO.classList.add("container-video-show");
 }
 
 function hiddenControls() {
-  CONTENEDOR__VIDEO.classList.remove('container-video-show');
+  CONTENEDOR__VIDEO.classList.remove("container-video-show");
 }
 
 function activeControls(ev) {
   const ID = ev.target.id;
-  
-  if (ID === 'play' || ID === 'video') {
+
+  if (ID === "play" || ID === "video") {
     VIDEO.play();
   }
 
-  if (ID === 'pause') {
+  if (ID === "pause") {
     VIDEO.pause();
   }
 
-  if (ID === 'mute') {
+  if (ID === "mute") {
     VIDEO.muted = true;
   }
 
-  if (ID === 'nomute') {
+  if (ID === "nomute") {
     VIDEO.muted = false;
   }
-
 }
 
 function startVideo() {
-  CONTENEDOR__VIDEO.addEventListener('mouseover', showControls, false);
-  CONTENEDOR__VIDEO.addEventListener('mouseout', hiddenControls, false);
-  CONTENEDOR__VIDEO.addEventListener('click', activeControls, false);
-  return console.log('Cargo...');
+  CONTENEDOR__VIDEO.addEventListener("mouseover", showControls, false);
+  CONTENEDOR__VIDEO.addEventListener("mouseout", hiddenControls, false);
+  CONTENEDOR__VIDEO.addEventListener("click", activeControls, false);
+  return console.log("Cargo...");
 }
 
-window.addEventListener('load', startVideo, false);
+window.addEventListener("load", startVideo, false);
