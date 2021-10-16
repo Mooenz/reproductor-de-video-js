7b7e9dd98251a6501de3390fce2eb92b645504ef
Mooenz <Mooenz@users.noreply.github.com>
2021-10-16 00:44:53 +0000

    deploy: 0679560ea114a9aee85d4222ca00f300bfb816d6

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,40 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const CONTENEDOR__VIDEO = document.querySelector('.main-container__video');
+const VIDEO = document.querySelector('.video');
 
-console.log('Happy hacking :)')
+function showControls() {
+  CONTENEDOR__VIDEO.classList.add('container-video-show');
+}
+
+function hiddenControls() {
+  CONTENEDOR__VIDEO.classList.remove('container-video-show');
+}
+
+function activeControls(ev) {
+  const ID = ev.target.id;
+  
+  if (ID === 'play' || ID === 'video') {
+    VIDEO.play();
+  }
+
+  if (ID === 'pause') {
+    VIDEO.pause();
+  }
+
+  if (ID === 'mute') {
+    VIDEO.muted = true;
+  }
+
+  if (ID === 'nomute') {
+    VIDEO.muted = false;
+  }
+
+}
+
+function startVideo() {
+  CONTENEDOR__VIDEO.addEventListener('mouseover', showControls, false);
+  CONTENEDOR__VIDEO.addEventListener('mouseout', hiddenControls, false);
+  CONTENEDOR__VIDEO.addEventListener('click', activeControls, false);
+  return console.log('Cargo...');
+}
+
+window.addEventListener('load', startVideo, false);
