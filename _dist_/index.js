34c4796bff11928a7f9041708b17a39d3cc4aa6f
Mooenz <Mooenz@users.noreply.github.com>
2021-10-17 23:07:24 +0000

    deploy: 57a41382f2ad5c2751f3132288d432ad57b8fa92

--- _dist_/index.js
+++ _dist_/index.js
@@ -10,34 +10,22 @@ const EXPAND = document.querySelector(".expand");
 const COMPRESS = document.querySelector(".compress");
 const DURATION = document.querySelector(".duration");
 
-function showControls() {
-  CONTENEDOR__VIDEO.classList.toggle("container-video-show");
-}
+const showControls = () => CONTENEDOR__VIDEO.classList.toggle("show-controls");
 
-function hiddenControls() {
-  CONTENEDOR__VIDEO.classList.toggle("container-video-show");
-}
+const hiddenControls = () => CONTENEDOR__VIDEO.classList.toggle("show-controls");
 
 function activeControls(ev) {
   const ID = ev.target.id;
 
   if (ID === "play" || ID === "video" || ID === "pause") {
-    if (VIDEO.paused === true) {
-      VIDEO.play();
-    } else {
-      VIDEO.pause();
-    }
+    VIDEO.paused === true ? VIDEO.play() : VIDEO.pause();
 
     PLAY.classList.toggle("hidden-icon");
     PAUSE.classList.toggle("show-icon");
   }
 
   if (ID === "mute" || ID === "nomute") {
-    if (VIDEO.muted === true) {
-      VIDEO.muted = false;
-    } else {
-      VIDEO.muted = true;
-    }
+    VIDEO.muted === true ? VIDEO.muted = false : VIDEO.muted = true
 
     MUTE.classList.toggle("hidden-icon");
     NOMUTE.classList.toggle("show-icon");
@@ -45,26 +33,23 @@ function activeControls(ev) {
 
 
   if (ID === "loop") {
-    if (VIDEO.loop === true) {
-      VIDEO.loop = false;
-    } else {
-      VIDEO.loop = true;
-    }
+    VIDEO.loop === true ? VIDEO.loop = false : VIDEO.loop = true
+
     LOOP.classList.toggle("active");
   }
 
   if (ID === "expand" || ID === "compress") {
-
     MAIN__CONTAINER.classList.toggle("expand-video");
     EXPAND.classList.toggle("hidden-icon");
     COMPRESS.classList.toggle("show-icon");
   }
 }
 
-function duration() {
-  const DURATION_VIDEO = Math.floor(VIDEO.duration);
-  const MIN_VIDEO = Math.floor(DURATION_VIDEO / 100);
-  const SEC_VIDEO = Math.floor(DURATION_VIDEO - 200);
+async function duration() {
+  const DURATION_VIDEO = await Math.floor(VIDEO.duration);
+  const MIN_VIDEO = await Math.floor(DURATION_VIDEO / 100);
+  const SEC_VIDEO = await Math.floor(DURATION_VIDEO - 200);
+
   return DURATION.innerHTML = `${MIN_VIDEO}:${SEC_VIDEO}`;
 }
 
@@ -73,6 +58,7 @@ function startVideo() {
   CONTENEDOR__VIDEO.addEventListener("mouseout", hiddenControls, false);
   CONTENEDOR__VIDEO.addEventListener("click", activeControls, false);
   duration();
+  
   return console.log("Cargo...");
 }
 
